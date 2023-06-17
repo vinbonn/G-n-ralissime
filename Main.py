00cc1d6ccf4334cde50b3a5833624c06fb7497b7
Vincebon <vbonnet@enssat.fr>
2023-06-17 02:22:44 +0200

    création fenetre

---
+++ Main.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+root = tk.Tk()  # Crée une nouvelle fenêtre
+root.title("Généralissime")  # Définit le titre de la fenêtre
+
+# Définit la taille de la fenêtre (largeur x hauteur)
+root.geometry("1600x1200")
+
+root.mainloop()  # Démarre la boucle d'événements
